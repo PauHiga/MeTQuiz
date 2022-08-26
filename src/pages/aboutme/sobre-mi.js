1690d9cf0d05f166c4131ba2631b6944978b5a76
Paula <p.gabriela.higa@gmail.com>
2022-08-26 20:42:08 -0300

    google fonts link moved to html

--- src/pages/aboutme/sobre-mi.js
+++ src/pages/aboutme/sobre-mi.js
@@ -10,14 +10,10 @@ function SobreMi(){
                 <div className="text-container">
                     <h1 className='purple-grey'>Sobre Mi</h1>
                     <h2>Hola, Soy Paula!</h2>
-
                     <p>Estoy muy entusiasmada de empezar mi propia aventura en programación!</p>
                     <p>He estado estudiando <span className='purple-grey'>html, css</span> y <span className='purple-grey'>javascript</span>. Este es mi primer <span className='purple-grey'>proyecto en React</span> bajo la guía del equipo de Mujeres en Tecnología.<br/>
                     Espero poder aumentar mis conocimientos, aprender y crecer en este nuevo camino!</p>
-
                     <p>Tengo un <span className='purple-grey'>gatito adorable</span> que gusta de arañar el sofa cuando cree que no lo estoy viendo. Cuando no estoy trabajando o estudiando, me gusta  <span className='purple-grey'>hacer ilustraciones</span>. También me gusta la <span className='purple-grey'>jardinería</span>, espero que mi planta de zarzamora crezca bien este año!</p>
-                    
-
                     <div className='footer'>
                         <div className='links'>
                             <a href="https://www.linkedin.com/in/paula-gabriela-higa-214690249/"><span className='icon-social'><FaLinkedin/></span></a>
