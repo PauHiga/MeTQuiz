a02f8beb4fbd14ccf9f91cb54ec8e46ec5c5aaad
Paula <p.gabriela.higa@gmail.com>
2022-09-17 22:21:40 -0300

    added background

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,11 @@
 import Button from "../../componentes/Button"
 import { useNavigate} from 'react-router-dom';
+import './index.css'
 
 function Home(){
     const navigate = useNavigate()
     return (
-        <section className="hero section is-fullheight">
+        <section className="hero section is-fullheight background">
             <div className="hero-body is-justify-content-center is-flex-direction-column">
                 <Button text="MetCamp Quiz" onClick={()=> navigate("/game")}/>
                 <Button text="Sobre Mí" onClick={()=> navigate("/sobre-mi")}/>
